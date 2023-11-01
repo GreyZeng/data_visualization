d151a8ddf5cde342e3ffd01c9765deb1a8c91d97
GreyZeng <410486047@qq.com>
2023-11-01 08:50:03 +0800

    update scatter graph

--- scatter_squares.py
+++ scatter_squares.py
@@ -8,7 +8,11 @@ plt.style.use("seaborn-v0_8")
 # 创建子图
 fig, ax = plt.subplots()
 # 设置单个散点坐标
-ax.scatter(2, 4, s=200)
+# ax.scatter(2, 4, s=200)
+# 批量设置散点
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+ax.scatter(x_values, y_values, s=200)
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
