cabc785959ef634bad7c6023848d70458bd73ebf
GreyZeng <410486047@qq.com>
2023-11-02 15:29:55 +0800

    update code

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 """绘制散点图"""
 # 通过这句话查看散点图样式有哪些
 # print(plt.style.available)
@@ -10,12 +12,14 @@ fig, ax = plt.subplots()
 # 设置单个散点坐标
 # ax.scatter(2, 4, s=200)
 # 批量设置散点
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
-ax.scatter(x_values, y_values, s=200)
+
+ax.scatter(x_values, y_values, s=50)
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 # 设置刻度标记的样式
 ax.tick_params(labelsize=14)
+
+# 设置每个坐标轴的取值范围
+ax.axis([0, 1100, 0, 1_100_000])
 plt.show()
