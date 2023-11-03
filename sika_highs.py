ea21102adc4fce6b3404e1046176f4e1c27ffa53
GreyZeng <410486047@qq.com>
2023-11-03 16:53:44 +0800

    update code

--- sika_highs.py
+++ sika_highs.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import csv
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 path = Path('weather_data/sitka_weather_07-2021_simple.csv')
 lines = path.read_text().splitlines()
@@ -11,20 +12,22 @@ header_row = next(reader)
 # enumerate 方法用于获取每个元素的索引和值
 # for index, column_header in enumerate(header_row):
 #     print(index, column_header)
-# 提取最高温度
-highs = []
+# 提取日期和最高温度
+dates, highs = [], []
 for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    dates.append(current_date)
     highs.append(high)
-print(highs)
 # print(header_row)
 # 根据最高温度绘图
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(highs, color='red')
+ax.plot(dates, highs, color='red')
 # 设置绘图格式
 ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(labelsize=16)
 plt.show()
