26c1e780ffe3cd575e687e072773a65bdf181ce6
GreyZeng <410486047@qq.com>
2023-11-03 16:36:52 +0800

    update code

--- sika_highs.py
+++ sika_highs.py
@@ -6,4 +6,8 @@ lines = path.read_text().splitlines()
 
 reader = csv.reader(lines)
 header_row = next(reader)
-print(header_row)
+
+# enumerate 方法用于获取每个元素的索引和值
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
+# print(header_row)
