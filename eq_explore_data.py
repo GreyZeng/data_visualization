f5c7b88f735f48d54042b9ac24d9f65b878a1a7f
GreyZeng <410486047@qq.com>
2023-11-03 20:16:26 +0800

    update

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -13,9 +13,19 @@ path.write_text(readable_contents)
 
 # 查看数据集中的所有地震
 all_eq_dicts = all_eq_data['features']
-mags = []
+mags, titles, lons, lats = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
+    title = eq_dict['properties']['title']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+
     mags.append(mag)
+    titles.append(title)
+    lons.append(lon)
+    lats.append(lat)
 # 打印前十次地震的震级
 print(mags[:10])
+print(titles[:2])
+print(lons[:5])
+print(lats[:5])
