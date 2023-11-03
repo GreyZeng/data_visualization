b80f2581cc5eb27f355d759e924e0c22e1aee8c3
GreyZeng <410486047@qq.com>
2023-11-03 17:26:07 +0800

    update code

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -1,13 +1,35 @@
 from pathlib import Path
 import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 path = Path('weather_data/death_valley_2021_simple.csv')
 
 lines = path.read_text().splitlines()
-
 reader = csv.reader(lines)
+header_low = next(reader)
+dates, highs, lows = [], [], []
+for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    try:
+        high = int(row[3])
+        low = int(row[4])
+    except ValueError:
+        print(f"Missing data for {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+# 根据最高温度绘图
+plt.style.use('seaborn-v0_8')
+fig, ax = plt.subplots()
+ax.plot(dates, highs, color='red', alpha=0.5)
+ax.plot(dates, lows, color='blue', alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+# 设置绘图格式
+title = "Daily High and Low Temperatures, 2021\nDeath Valley, CA"
+ax.set_title(title, fontsize=20)
+ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 
-header_row = next(reader)
-
-for index, column_header in enumerate(header_row):
-    print(index, column_header)
+plt.show()
