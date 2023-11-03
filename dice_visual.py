6eeb8e44590bfcd9d2c70467f1af38aabbee80b9
GreyZeng <410486047@qq.com>
2023-11-03 16:01:50 +0800

    update code

---
+++ dice_visual.py
@@ -0,0 +1,26 @@
+from die import Die
+import plotly.express as px
+
+die_1 = Die()
+die_2 = Die()
+
+results = []
+
+for roll_num in range(1000):
+    result = die_1.roll() + die_2.roll()
+    results.append(result)
+
+# 分析结果
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result + 1)
+for value in poss_results:
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# 对结果进行可视化
+# install ploty, pandas 两个模块
+title = "Results of Rolling Two D6 Dice 1,000 times"
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.show()
