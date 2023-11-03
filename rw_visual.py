8e5d2046b6b555b38c9cacec8e4f68a6671ee437
GreyZeng <410486047@qq.com>
2023-11-03 15:27:44 +0800

    update code

--- rw_visual.py
+++ rw_visual.py
@@ -13,6 +13,9 @@ while True:
     # 突出起点和终点
     ax.scatter(0, 0, c='green', edgecolors='none', s=100)
     ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     plt.show()
     keep_running = input("Make another walk? (y/n): ")
     if keep_running == 'n':
