bf7b286ec80fb7eddf242616115495d4bc917351
GreyZeng <410486047@qq.com>
2023-11-03 20:23:00 +0800

    update code

---
+++ eq_world_map.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+import json
+import plotly.express as px
+
+# 将数据转换为字符串读取并转换为Python对象
+path = Path('eq_data/eq_data_1_day_m1.geojson')
+contents = path.read_text()
+all_eq_data = json.loads(contents)
+
+# 将数据文件转换为更容易阅读的版本
+path = Path('eq_data/readable_eq_data.geojson')
+readable_contents = json.dumps(all_eq_data, indent=4)
+path.write_text(readable_contents)
+
+# 查看数据集中的所有地震
+all_eq_dicts = all_eq_data['features']
+mags, titles, lons, lats = [], [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    title = eq_dict['properties']['title']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+
+    mags.append(mag)
+    titles.append(title)
+    lons.append(lon)
+    lats.append(lat)
+# 打印前十次地震的震级
+print(mags[:10])
+print(titles[:2])
+print(lons[:5])
+print(lats[:5])
+fig = px.scatter(
+    x=lons,
+    y=lats,
+    labels={'x': '经度', 'y': '纬度'},
+    range_x=[-200, 200],
+    range_y=[-90, 90],
+    width=800,
+    height=800,
+    title='全球地震散点图'
+)
+# fig.write_html('global_earthquakes.html')
+fig.show()
