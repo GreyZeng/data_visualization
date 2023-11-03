4437e5de5a204c8622bb797ba64eee7838c92a48
GreyZeng <410486047@qq.com>
2023-11-03 19:06:05 +0800

    update

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -7,7 +7,15 @@ contents = path.read_text()
 all_eq_data = json.loads(contents)
 
 # 将数据文件转换为更容易阅读的版本
-
 path = Path('eq_data/readable_eq_data.geojson')
 readable_contents = json.dumps(all_eq_data, indent=4)
 path.write_text(readable_contents)
+
+# 查看数据集中的所有地震
+all_eq_dicts = all_eq_data['features']
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+# 打印前十次地震的震级
+print(mags[:10])
