633ea74a874f49f975ed944c13e8139762e75890
GreyZeng <410486047@qq.com>
2023-11-05 16:47:54 +0800

    update code

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -19,6 +19,9 @@ for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
 # 可视化
+title = "Most-Starred Python Projects on Github"
+labels = {'x': 'Repository', 'y': 'Stars'}
 
-fig = px.bar(x=repo_names, y=stars)
+fig = px.bar(x=repo_names, y=stars, title=title, labels=labels)
+fig.update_layout(title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20)
 fig.show()
